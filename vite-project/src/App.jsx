1a7e479f15a3684407da7615d5707cf150faff5c
cheboksun <cheboksun2007@gmail.com>
2026-07-15 12:37:28 +0900

    [Fix] app.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,4 @@
 import HyunwooPage from "./hyunwoo/HyunwooPage";
-
-// function App() {
-//   return <HyunwooPage />;
-// }
 import React from "react";
 import MainPage from "./boksun/MainPage";
 const App = () => {
